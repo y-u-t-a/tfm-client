54001d5b617bdc334c8e8f2bd3f4e61e04c25bf2
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2026-02-07 01:11:53 +0900

    エピソード一覧に再生ボタンを追加

--- app/components/EpisodeList.vue
+++ app/components/EpisodeList.vue
@@ -4,26 +4,35 @@
       v-for="episode in episodes"
       :key="episode.audio"
       variant="outline"
-      class="flex flex-col justify-between overflow-hidden"
+      class="flex flex-col justify-between"
     >
       <template #header>
-        <NuxtLink :to="`/${programId}/episode/${episode.id}`">
-          {{ episode.title }}
-        </NuxtLink>
+        {{ episode.title }}
       </template>
       <div class="flex items-start gap-4 mb-2">
         <img
           v-if="episode.thumbnail"
           :src="episode.thumbnail"
           alt="thumbnail"
-          class="w-50 h-50 object-cover rounded"
+          class="w-50 h-30 object-cover rounded"
         />
-        <p class="line-clamp-3 flex-1">
+        <p class="h-30 overflow-auto">
           {{ episode.description }}
         </p>
       </div>
-      <small>
-        長さ: {{ episode.length }} / 公開日: {{ new Date(episode.publishedAt).toLocaleString() }}
+      <small class="flex items-center gap-2">
+        <span>
+          長さ: {{ episode.length }} / 公開日: {{ new Date(episode.publishedAt).toLocaleString() }}
+        </span>
+        <UButton
+          :href="episode.audio"
+          target="_blank"
+          label="Play"
+          color="neutral"
+          variant="soft"
+          trailing-icon="i-lucide-external-link"
+          size="sm"
+        />
       </small>
     </UCard>
   </div>
