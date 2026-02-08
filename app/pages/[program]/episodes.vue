32c08f7329f5acd106788566ba45d4475e925fac
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2026-02-08 12:26:20 +0900

    エピソード一覧で番組名を表示する

--- app/pages/[program]/episodes.vue
+++ app/pages/[program]/episodes.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
-    <h1>{{ id }}</h1>
     <AppLoading :loading="pending" />
-    <EpisodeList
-      v-if="data"
-      :program-id="id"
-      :episodes="data.episodes"
-    />
+    <template v-if="data">
+      <h1 class="text-2xl font-bold mb-4">
+        {{ data.title }}
+      </h1>
+      <EpisodeList
+        :program-id="id"
+        :episodes="data.episodes"
+      />
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-useHead({
-  title: 'エピソード',
-})
-
 const route = useRoute('program-episodes')
 const id = route.params.program as string
 
 const { execute, data, pending } = useFetch(`/api/${id}/episodes`, { immediate: false })
+
+useHead({
+  title: computed(() => data.value ? `${data.value.title} - エピソード` : 'エピソード'),
+})
 
 onMounted(() => {
   execute()
