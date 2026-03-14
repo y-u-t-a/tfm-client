cca37a66b6fc17c67fdf481f80e1638215128804
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2026-03-15 04:57:03 +0900

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt()
   .override('nuxt/stylistic', {
     rules: {
+      // { ... } が1行に収まるブロックを許可。
       '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
     },
   })
